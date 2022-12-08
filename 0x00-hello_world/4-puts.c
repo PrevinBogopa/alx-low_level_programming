97cc2b8749d5fed298db24f7ee4d00e941864cc0
PrevinBogopa <previnbogopa@icloud.com>
2022-12-08 13:43:39 -0800

    don

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
-{
-/*this progreamm*/
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
+i{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 
 }
