2d3a902b65ebcc2c3db7e120a064165ec34e2493
PrevinBogopa <previnbogopa@icloud.com>
2022-12-08 12:59:03 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*This is the comment line
+ the printf displays output to user
+ * Theprogramm shoudl return 0
+ * */
+
+int main (void)
+{
+printf ("with proper grammar, but the outcome is a piece of art,\n")
+
+	return (0);
+}
