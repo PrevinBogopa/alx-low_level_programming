595e2fbfd99a1ac64c1bd1f3ed93074cf1704e8b
PrevinBogopa <previnbogopa@icloud.com>
2022-12-13 07:37:55 -0800

    prev

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * print_alphabets - print alphabets in lowercase
+ * Return: Always 0
+ */
+
+void print_alphbet(void)
+{
+	char alphabets;
+
+	for (alphabets = 'a' ; alphabets <= 'z' ; alphabets++)
+	{
+		_putchar(alphabets);
+		_putchar(\'n')
+	}
+}
