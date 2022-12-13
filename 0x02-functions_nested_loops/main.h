ce23a4f18f12da5db61f3d41b5ca03e82d15e483
PrevinBogopa <previnbogopa@icloud.com>
2022-12-13 06:06:19 -0800

    header

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _putchar - gives characters v to the stdout
+ * @v: the character it displays
+ *
+ * Return: if success it should be 1 else -1
+ */
+
+int _putchar(char v);
+
+#endif
