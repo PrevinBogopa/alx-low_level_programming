6dddbdfed57fb113baf53e29db10a342194d5359
PrevinBogopa <previnbogopa@icloud.com>
2022-12-15 18:01:24 -0800

    debug

---
+++ 0x03-debugging/1.main.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+
+/**
+ * main - makes infinite loop
+ * Return: 0;
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i=0;
+	print("Infinite loop avoided: \\o/\n");
+
+	return (0);
+}
